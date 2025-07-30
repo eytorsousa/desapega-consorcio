c256f394c13f367e4d8b16b19429e43aa2afc25d
eytorlima <eytorlima@outlook.com>
2025-07-29 23:49:43 -0300

    feat: details animation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const backToTopButton = document.querySelector('.back_to_top');
 const header = document.querySelector('pre-header');
+const details = document.querySelectorAll('details');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 750) {
@@ -9,3 +10,12 @@ window.addEventListener('scroll', () => {
     }
 });
 
+details.forEach(detail => {
+    detail.addEventListener('click', () => {
+        details.forEach(d => {
+            if (d !== detail) {
+                d.open = false;
+            }
+        });
+    });
+});
